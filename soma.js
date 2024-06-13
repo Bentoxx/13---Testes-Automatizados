481b66bd1f7e7b4a0f94cf18192f3d926d6595bb
Bento Netto <bentoeemanuele@gmail.com>
2024-06-13 16:18:31 -0300

    Commit Inicial

---
+++ soma.js
@@ -0,0 +1,14 @@
+//ComonsJS
+// function soma (numero1, numero2){
+//     return numero1 + numero2
+// }
+// const nome = 'Bento'
+// module.exports = {
+//     soma,
+//     nome,
+// }
+// ES modules
+function soma (numero1, numero2){
+    return numero1 + numero2
+}
+module.exports = soma
