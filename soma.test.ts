867c3e615cc60e5a57c12944e15da5b5ad52997f
Bento Netto <bentoeemanuele@gmail.com>
2024-06-13 20:38:18 -0300

    implementação da função subtração

--- soma.test.ts
+++ soma.test.ts
@@ -2,11 +2,11 @@ import soma from './soma'
 
 describe('Testes de soma', ()=>{
     it ('Deve verificar o resultado de uma soma', () => {
-        expect(soma(3,8)).toBe(11)
+        expect(soma(3,8)).toBeCloseTo(11)
     })
 
     it ('Deve verificar o resultado incorreto de uma soma', () => {
-        expect(soma(3,8)).not.toBe(12)
+        expect(soma(3,8)).not.toBeCloseTo(12)
     })
 })
 
