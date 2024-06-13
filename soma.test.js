2870c886d6b97c1aeaa1f0d18070266d2949c426
Bento Netto <bentoeemanuele@gmail.com>
2024-06-13 16:39:24 -0300

    Implementação do describe nos testes de soma

--- soma.test.js
+++ soma.test.js
@@ -1,5 +1,12 @@
 const soma = require('./soma')
 
-test ('Deve verificar o resultado de uma soma', () => {
-    expect(soma(3,8)).toBe(11)
+describe('Testes de soma', ()=>{
+    it ('Deve verificar o resultado de uma soma', () => {
+        expect(soma(3,8)).toBe(11)
+    })
+
+    it ('Deve verificar o resultado incorreto de uma soma', () => {
+        expect(soma(3,8)).not.toBe(12)
+    })
 })
+
